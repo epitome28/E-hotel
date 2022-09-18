968af6e85080d2c52e961997ef91cc13bd19d6f8
HP <akinlabisamson15@gmail.com>
2022-09-18 05:46:38 -0700

    laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::view('home', 'home');
+Route::view('about', 'about');
+Route::view('contact', 'contact');
 require __DIR__.'/auth.php';
